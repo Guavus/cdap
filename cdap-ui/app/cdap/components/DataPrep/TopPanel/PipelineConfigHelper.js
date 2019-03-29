bca387379551d04d7115837906f62c97390fbf50
avantika.karwasara <avantika.karwasara@guavus.com>
2019-03-29 13:41:04 +0530

    fix(adls-scope): added scope for adls system
    RAFD-386

--- cdap-ui/app/cdap/components/DataPrep/TopPanel/PipelineConfigHelper.js
+++ cdap-ui/app/cdap/components/DataPrep/TopPanel/PipelineConfigHelper.js
@@ -345,6 +345,7 @@ function constructAdlsSource(artifactsList, adlsInfo) {
   }
 
   batchArtifact.version = '[0.9.2, 9.0.0)';
+  batchArtifact.scope = SCOPES.SYSTEM;
   let plugin = objectQuery(adlsInfo, 'values', 0);
 
   let pluginName = Object.keys(plugin)[0];
