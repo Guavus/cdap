5bd34e341fe84ca4eb0b77e0b84bdae33b8c045f
avantika <avantika.karwasara@guavus.com>
2019-08-26 12:03:14 +0530

    RAFD-919: removed extra transports

--- cdap-ui/app/services/cask-angular-socket-datasource/socket.js
+++ cdap-ui/app/services/cask-angular-socket-datasource/socket.js
@@ -40,8 +40,13 @@ angular.module(PKG.name+'.services')
       $log.log('[mySocket] init');
 
       attempt = attempt || 1;
-      socket = new SockJS(self.prefix);
-
+      socket = new SockJS(self.prefix, {} ,
+        {
+          transports:[
+            'websocket',
+            'websocket-raw'
+          ]
+        });
       socket.onmessage = function (event) {
         try {
           var data = JSON.parse(event.data);
